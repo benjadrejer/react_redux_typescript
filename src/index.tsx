5f52584ff5f8c1e5e487eb9c7f5c6a5a35013a19
Benjamin Drejer <benjadrejer@gmail.com>
2020-08-13 20:46:18 +0200

    Finished

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.querySelector('#root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// interface AppProps {
+//   color?: string;
+// }
+
+// // Hook Typescript declaration
+// const App: React.FC<AppProps> = (props) => {
+//   return <div>{props.color}</div>
+// }
+
+// // Functional Component Typescript declaration
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>
+// }
+
+
+// Class-based app Typescript declaration
+// interface AppState {
+//   counter: number;
+// }
+
+// class App extends React.Component<AppProps, AppState> {
+//   constructor(props: AppProps) {
+//     super(props);
+
+//     this.state = { counter: 0 };
+//   }
+
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   }
+
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   }
+
+//   render() {
+//     return (<div>
+//       <button onClick={this.onIncrement}>Increment</button>
+//       <button onClick={this.onDecrement}>Decrement</button>
+//       {this.state.counter}
+//     </div>)
+//   };
+// };
+
+// ReactDOM.render(<App color="red" />, document.querySelector('#root'));
